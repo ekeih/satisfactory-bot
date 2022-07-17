6b551ac4d784da35660cce53ac5ad4a1132bee08
Max Rosin <git@hackrid.de>
2022-07-17 02:05:44 +0200

    Add --version flag to print bot version

--- satisfactory/cli.py
+++ satisfactory/cli.py
@@ -1,4 +1,5 @@
 import logging
+from importlib import metadata
 
 import click
 from prometheus_client import start_http_server
@@ -20,11 +21,17 @@ CONTEXT_SETTINGS = {
 @click.option("-r", "--repository", "repository", required=True, default="ekeih/satisfactory-bot", type=str, help="GitHub repository")
 @click.option("-a", "--metrics-address", required=True, default="0.0.0.0", type=str, help="IP address to serve metrics on")
 @click.option("-p", "--metrics-port", required=True, default=8000, type=int, help="Port to serve metrics on")
-def cli(bot_token: str, github_token: str, chat_id: int, repository: str, metrics_address: str, metrics_port: int) -> None:
-    start_http_server(port=metrics_port, addr=metrics_address)
-    github_client = satisfactory.git.Git(github_token, repository)
-    bot = satisfactory.chat.Bot(bot_token=bot_token, chat_id=chat_id, github_client=github_client)
-    bot.start()
+@click.option("-v", "--version", is_flag=True, type=bool, help="Show version and exit")
+def cli(bot_token: str, github_token: str, chat_id: int, repository: str, metrics_address: str, metrics_port: int, version: bool) -> None:
+    installed_version=metadata.version('satisfactory_bot')
+    if version:
+        print(installed_version)
+    else:
+        logging.info("Running version %s", installed_version)
+        start_http_server(port=metrics_port, addr=metrics_address)
+        github_client = satisfactory.git.Git(github_token, repository)
+        bot = satisfactory.chat.Bot(bot_token=bot_token, chat_id=chat_id, github_client=github_client)
+        bot.start()
 
 
 if __name__ == "__main__":
