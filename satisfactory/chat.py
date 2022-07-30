3846cc37899ba97e1097104391cfcdd85b491ca0
Max Rosin <git@hackrid.de>
2022-07-30 21:30:59 +0200

    Send feedback for commands without results
    Fixes https://github.com/ekeih/satisfactory-bot/issues/18

--- satisfactory/chat.py
+++ satisfactory/chat.py
@@ -56,18 +56,24 @@ class Bot:
     def check_images(self, context: CallbackContext) -> None:
         versions = satisfactory.game.get_versions()
         existing_git_tags = [tag.name for tag in self.github_client.get_tags()]
+        new_versions_count = 0
         for version, values in versions.items():
             version_tag = "server-%s/%s-%s" % (version, values["timeupdated"].strftime("%Y.%m.%d"), values["buildid"])
             if version_tag in existing_git_tags:
                 logging.info("Git tag %s for %s already exists, doing nothing", version_tag, version)
             else:
                 logging.info("Tagging new %s version: %s", version, version_tag)
+                new_versions_count += 1
                 self.github_client.create_tag(version_tag)
                 context.bot.send_message(chat_id=self.chat_id, text="Tagged new release <code>%s</code>\n\n%s" % (version_tag, satisfactory.game.get_random_quote()), parse_mode=ParseMode.HTML)
+        if new_versions_count == 0 and not context.job:
+            # A user triggered the check manually, inform them that there was no new version found.
+            context.bot.send_message(chat_id=self.chat_id, text="No newer versions available, doing nothing.\n\n%s" % (satisfactory.game.get_random_quote()), parse_mode=ParseMode.HTML)
 
     @Summary("satisfactory_bot_check_news", "Time spent running check_news").time()
     def check_news(self, context: CallbackContext) -> None:
-        for news in satisfactory.game.get_patchnotes():
+        recent_news = satisfactory.game.get_patchnotes()
+        for news in recent_news:
             if len(news) <= constants.MAX_MESSAGE_LENGTH:
                 context.bot.send_message(chat_id=self.chat_id, text=news, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
             else:
@@ -87,3 +93,6 @@ class Bot:
                         break
                 for part in parts:
                     context.bot.send_message(chat_id=self.chat_id, text=part, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+        if len(recent_news) == 0 and not context.job:
+            # A user triggered the check manually, inform them that there are no recent news available.
+            context.bot.send_message(chat_id=self.chat_id, text="No recent news found.\n\n%s" % (satisfactory.game.get_random_quote()), parse_mode=ParseMode.HTML)
