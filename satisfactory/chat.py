a43a36ad19b92fc3ccabe31c280d4df9e4c95eed
Markus Ressel <mail@markusressel.de>
2023-09-02 18:33:10 +0200

    don't register "/images" command if github token wasn't specified

--- satisfactory/chat.py
+++ satisfactory/chat.py
@@ -18,7 +18,8 @@ class Bot:
     youtube_client: satisfactory.youtube.Youtube or None = None
     chat_id = None
 
-    def __init__(self, bot_token: str, chat_id: int, github_client: satisfactory.git.Git or None, youtube_client: satisfactory.youtube.Youtube or None):
+    def __init__(self, bot_token: str, chat_id: int, github_client: satisfactory.git.Git or None,
+                 youtube_client: satisfactory.youtube.Youtube or None):
         self.chat_id = chat_id
         self.github_client = github_client
         self.youtube_client = youtube_client
@@ -27,7 +28,8 @@ class Bot:
 
         self._app.add_handler(CommandHandler("start", self.start_handler, Chat(self.chat_id)))
         self._app.add_handler(CommandHandler("quote", self.quote_handler, Chat(self.chat_id)))
-        self._app.add_handler(CommandHandler("images", self.check_images_handler, Chat(self.chat_id)))
+        if self.github_client is not None:
+            self._app.add_handler(CommandHandler("images", self.check_images_handler, Chat(self.chat_id)))
         self._app.add_handler(CommandHandler("news", self.check_news_handler, Chat(self.chat_id)))
 
     def start(self) -> None:
@@ -36,7 +38,7 @@ class Bot:
         self._app.job_queue.run_repeating(self.check_news_timer, interval=60 * 60, first=30)
         if self.youtube_client is not None:
             self._app.job_queue.run_repeating(self.check_videos, interval=60 * 60, first=15)
-        self._app.job_queue.run_once(self.send_random_quote_timer, random.randrange(60*60*24, 60*60*24*7, 1))
+        self._app.job_queue.run_once(self.send_random_quote_timer, random.randrange(60 * 60 * 24, 60 * 60 * 24 * 7, 1))
         self._app.run_polling()
 
     @Summary("satisfactory_bot_start_handler", "Time spent running start_handler").time()
@@ -74,7 +76,8 @@ class Bot:
     @Summary("satisfactory_bot_send_random_quote_timer", "Time spent running the send_random_quote_timer").time()
     async def send_random_quote_timer(self, context: CallbackContext):
         await self.send_random_quote(context)
-        (self._app.job_queue.run_once(self.send_random_quote_timer, random.randrange(60*60*24, 60*60*24*7, 1)))
+        (self._app.job_queue.run_once(self.send_random_quote_timer,
+                                      random.randrange(60 * 60 * 24, 60 * 60 * 24 * 7, 1)))
 
     @Summary("satisfactory_bot_check_images", "Time spent running check_images").time()
     async def check_images(self, context: CallbackContext) -> None:
@@ -89,17 +92,21 @@ class Bot:
                 logging.info("Tagging new %s version: %s", version, version_tag)
                 new_versions_count += 1
                 self.github_client.create_tag(version_tag)
-                await context.bot.send_message(chat_id=self.chat_id, text="Tagged new release <code>%s</code>\n\n%s" % (version_tag, satisfactory.game.get_random_quote()), parse_mode=ParseMode.HTML)
+                await context.bot.send_message(chat_id=self.chat_id, text="Tagged new release <code>%s</code>\n\n%s" % (
+                version_tag, satisfactory.game.get_random_quote()), parse_mode=ParseMode.HTML)
         if new_versions_count == 0 and not context.job:
             # A user triggered the check manually, inform them that there was no new version found.
-            await context.bot.send_message(chat_id=self.chat_id, text="No newer versions available, doing nothing.\n\n%s" % (satisfactory.game.get_random_quote()), parse_mode=ParseMode.HTML)
+            await context.bot.send_message(chat_id=self.chat_id,
+                                           text="No newer versions available, doing nothing.\n\n%s" % (
+                                               satisfactory.game.get_random_quote()), parse_mode=ParseMode.HTML)
 
     @Summary("satisfactory_bot_check_news", "Time spent running check_news").time()
     async def check_news(self, context: CallbackContext) -> None:
         recent_news = satisfactory.game.get_patchnotes()
         for news in recent_news:
             if len(news) <= constants.MessageLimit.MAX_TEXT_LENGTH:
-                await context.bot.send_message(chat_id=self.chat_id, text=news, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+                await context.bot.send_message(chat_id=self.chat_id, text=news, parse_mode=ParseMode.HTML,
+                                               disable_web_page_preview=True)
             else:
                 parts = []
                 while len(news) > 0:
@@ -116,14 +123,18 @@ class Bot:
                         parts.append(news)
                         break
                 for part in parts:
-                    await context.bot.send_message(chat_id=self.chat_id, text=part, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+                    await context.bot.send_message(chat_id=self.chat_id, text=part, parse_mode=ParseMode.HTML,
+                                                   disable_web_page_preview=True)
         if len(recent_news) == 0 and not context.job:
             # A user triggered the check manually, inform them that there are no recent news available.
-            await context.bot.send_message(chat_id=self.chat_id, text="No recent news found.\n\n%s" % (satisfactory.game.get_random_quote()), parse_mode=ParseMode.HTML)
+            await context.bot.send_message(chat_id=self.chat_id,
+                                           text="No recent news found.\n\n%s" % (satisfactory.game.get_random_quote()),
+                                           parse_mode=ParseMode.HTML)
 
     @Summary("satisfactory_bot_send_random_quote", "Time spent running send_random_quote").time()
     async def send_random_quote(self, context: CallbackContext) -> None:
-        await context.bot.send_message(chat_id=self.chat_id, text=satisfactory.game.get_random_quote(), parse_mode=ParseMode.HTML)
+        await context.bot.send_message(chat_id=self.chat_id, text=satisfactory.game.get_random_quote(),
+                                       parse_mode=ParseMode.HTML)
 
     @Summary("satisfactory_bot_check_videos", "Time spent running check_videos").time()
     async def check_videos(self, context: CallbackContext) -> None:
